a4b28f37f54ff6d1155b2a0f60a3ac47274f3d73
Douglas Duarte <douglasduarte.eng@gmail.com>
2024-02-14 07:32:42 -0300

    feat<shadcn/ui>: config shadcn/ui.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import './global.css'
+import { Button } from "@/components/ui/button"
+
 
 export function App() {
 
   return (
-    <h1>Hello World</h1>
+    <div className='inset-64 flex'>
+      <Button variant="outline">Button</Button>
+    </div>
   )
 }
 
