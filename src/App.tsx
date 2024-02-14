64c5464cf6077bc82adf2f2589568f56cab4b5b3
Douglas Duarte <douglasduarte.eng@gmail.com>
2024-02-14 07:52:49 -0300

    feat<eslint>: config eslint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './global.css'
-import { Button } from "@/components/ui/button"
 
+import { Button } from '@/components/ui/button'
 
 export function App() {
-
   return (
-    <div className='inset-64 flex'>
+    <div className="flex">
       <Button variant="outline">Button</Button>
     </div>
   )
 }
-
